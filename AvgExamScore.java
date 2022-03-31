ae4ce08c585cbf2c76a40a5256c90a66f91358c1
Samuel Desalegn Tumdedo <64654098+samueldesalegn@users.noreply.github.com>
2022-03-30 22:36:17 -0500

    Update AvgExamScore.java

--- AvgExamScore.java
+++ AvgExamScore.java
@@ -6,35 +6,34 @@ public class AvgExamScore {
 	
 
 
-	  public static void main(String[] args) {
+   public static void main(String[] args) {
 
 
 
-    Scanner sc = new Scanner(System.in);
+   Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter your first Name: ");
-    String firstName = sc.nextLine();
+   System.out.println("Enter your first Name: ");
+   String firstName = sc.nextLine();
 
-		System.out.println("Enter your last Name: ");
-    String lastName = sc.nextLine();
+   System.out.println("Enter your last Name: ");
+   String lastName = sc.nextLine();
 
-		System.out.println("Enter your grade value for subject A: ");
-    double gradeOne = sc.nextDouble();
+   System.out.println("Enter your grade value for subject A: ");
+   double gradeOne = sc.nextDouble();
 
-		System.out.println("Enter your grade values for subject B: ");
-    double gradeTwo = sc.nextDouble();
+   System.out.println("Enter your grade values for subject B: ");
+   double gradeTwo = sc.nextDouble();
 
-		System.out.println("Enter your grade value for subject C: ");
-    double gradeThree = sc.nextDouble();
+   System.out.println("Enter your grade value for subject C: ");
+   double gradeThree = sc.nextDouble();
 
-		double avg = (gradeOne + gradeTwo + gradeThree)/3.0;
-
-		DecimalFormat df = new DecimalFormat("#.00");
+   double avg = (gradeOne + gradeTwo + gradeThree)/3.0;
+   DecimalFormat df = new DecimalFormat("#.00");
     
 		
 
-		System.out.println("Your full name is " + firstName + " " + lastName + "\n and the the average grade is " + df.format(avg) + "." );
+   System.out.println("Your full name is " + firstName + " " + lastName + "\n and the the average grade is " + df.format(avg) + "." );
 
-	}
+   }
 
 }
